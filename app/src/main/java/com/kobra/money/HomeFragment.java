909ea58efe612758eb2dfd03a1a45817b599f129
Mr-Kobra <red.kras.2013@gmail.com>
2022-03-24 19:47:20 +0300

    Create HomeFragment.java

---
+++ app/src/main/java/com/kobra/money/HomeFragment.java
@@ -0,0 +1,74 @@
+package com.kobra.money;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kobra.money.controller.OperationController;
+import com.kobra.money.view.OperationView;
+import com.kobra.money.view.adapter.ListOperationAdapter;
+
+import java.util.HashMap;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+    private Context context;
+    private View fragment;
+
+    private OperationController operationController;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+        operationController = new OperationController(context);
+        operationController.setItems(new HashMap<String, String>(){{
+            put("include_category", "1");
+            put("include_type", "1");
+        }});
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        fragment = inflater.inflate(R.layout.fragment_home, container, false);
+
+        OperationView operationView = new OperationView(context, fragment.findViewById(R.id.operationsList),
+                new ListOperationAdapter(context));
+        operationView.setEmptyNotify(fragment.findViewById(R.id.emptyOperation));
+        operationController.setView(operationView);
+        operationController.print();
+
+        return fragment;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+}
