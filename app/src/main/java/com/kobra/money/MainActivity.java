7cd36d9493565fe11a3cf6ec8ed4706954e05ceb
Mr-Kobra <red.kras.2013@gmail.com>
2022-03-24 21:03:03 +0300

    Add check auth on start activity

--- app/src/main/java/com/kobra/money/MainActivity.java
+++ app/src/main/java/com/kobra/money/MainActivity.java
@@ -4,16 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.kobra.money.controller.AuthController;
+import com.kobra.money.include.AppSettings;
+
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private AuthController authController;
+    private AppSettings appSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadFragment(HomeFragment.newInstance());
+        context = this;
+        authController = new AuthController(context);
+        appSettings = new AppSettings(context);
+
+        String token = appSettings.getProperty("token");
+        authController.isLogin(token, new AuthController.Event() {
+            @Override
+            public void onSuccess() {
+                loadFragment(HomeFragment.newInstance());
+            }
+
+            @Override
+            public void onError() {
+                //finish();
+            }
+        });
     }
 
     public void loadFragment(Fragment fragment) {
