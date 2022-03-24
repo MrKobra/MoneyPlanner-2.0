0738e0fdbb1d55a5582b7fa28098f5da1fa531b3
Mr-Kobra <red.kras.2013@gmail.com>
2022-03-24 19:07:17 +0300

    Create home fragment

--- app/src/main/java/com/kobra/money/MainActivity.java
+++ app/src/main/java/com/kobra/money/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kobra.money;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(HomeFragment.newInstance());
+    }
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_content, fragment);
+        fragmentTransaction.commit();
     }
 }
