a3c3d0209f46476fda15dbffa208cc777ac19e27
Mr-Kobra <red.kras.2013@gmail.com>
2022-03-25 11:21:01 +0300

    Add get/set in user model

--- app/src/main/java/com/kobra/money/model/UserModel.java
+++ app/src/main/java/com/kobra/money/model/UserModel.java
@@ -90,5 +90,45 @@ public class UserModel {
             password = item.getString("password");
             email = item.getString("email");
         }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 }
