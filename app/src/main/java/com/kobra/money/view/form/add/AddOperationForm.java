0174d5f2190ab8770021b7c4b1284b7567402f9d
Mr-Kobra <red.kras.2013@gmail.com>
2022-04-01 23:39:08 +0300

    Add select category method

--- app/src/main/java/com/kobra/money/view/form/add/AddOperationForm.java
+++ app/src/main/java/com/kobra/money/view/form/add/AddOperationForm.java
@@ -1,11 +1,19 @@
 package com.kobra.money.view.form.add;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.kobra.money.R;
 import com.kobra.money.include.Finder;
 import com.kobra.money.model.CategoryModel;
+import com.kobra.money.view.dialog.SelectCategoryDialog;
 import com.kobra.money.view.form.Form;
 import com.kobra.money.view.form.LoginForm;
 import com.kobra.money.view.table.CategoryTable;
@@ -14,9 +22,12 @@ import java.util.List;
 
 public class AddOperationForm extends Form {
     private CategoryTable categoryTable;
+    private LinearLayout selectCategoryButton;
+    private SelectCategoryDialog selectCategoryDialog;
 
     private AddOperationForm(Context context) {
         super(context);
+        initSelectCategoryDialog();
     }
 
     @Override
@@ -29,12 +40,14 @@ public class AddOperationForm extends Form {
         super.setFormView(formView);
         categoryTable = new CategoryTable(context, formView.findViewById(R.id.categoryTable));
         initFields();
+        initSelectCategoryButton();
     }
 
     public void setCategories(List<CategoryModel.Category> categories) {
         if(categoryTable != null) {
-            categoryTable.setCategories(categories);
-            categoryTable.print(10);
+            categoryTable.setCategories(categories.subList(0, 3));
+            categoryTable.print(0);
+            selectCategoryDialog.setCategories(categories);
             selectCategory();
         }
     }
@@ -47,7 +60,7 @@ public class AddOperationForm extends Form {
                 categoryTableItem.getView().setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        long categoryId = Long.parseLong(field.getValue());
+                        long categoryId = (field.getValue().isEmpty()) ? 0 : Long.parseLong(field.getValue());
                         CategoryModel.Category clickCategory = categoryTableItem.getCategory();
 
                         for(CategoryTable.CategoryTableItem categoryTableItem : categoryTableItems) {
@@ -60,6 +73,30 @@ public class AddOperationForm extends Form {
                             field.setValue(Long.toString(clickCategory.getId()));
                             categoryTableItem.setSelectedView(true);
                         }
+
+                        CardView categoryIcon = (CardView) selectCategoryButton.findViewById(R.id.categoryIcon);
+                        categoryIcon.setCardBackgroundColor(context.getColor(R.color.normal));
+                    }
+                });
+            }
+        }
+    }
+
+    private void initSelectCategoryButton() {
+        if(formView != null) {
+            selectCategoryButton = (LinearLayout) formView.findViewById(R.id.selectCategoryButton);
+            if(selectCategoryButton != null) {
+                ImageView buttonIcon = selectCategoryButton.findViewById(R.id.categoryImg);
+                buttonIcon.setImageResource(R.drawable.ic_other);
+                buttonIcon.setColorFilter(context.getColor(R.color.minor));
+
+                TextView buttonText = selectCategoryButton.findViewById(R.id.categoryTitle);
+                buttonText.setText(R.string.select_category_button);
+
+                selectCategoryButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        selectCategoryDialog.show();
                     }
                 });
             }
@@ -71,6 +108,38 @@ public class AddOperationForm extends Form {
             formFields.add(new FormField("category", "category_id"));
             formFields.add(new FormField("amount", "amount"));
         }
+    }
+
+    private void initSelectCategoryDialog() {
+        selectCategoryDialog = new SelectCategoryDialog(context);
+        selectCategoryDialog.setEvent(new SelectCategoryDialog.Event() {
+            @Override
+            public void onAccept() {
+                Long selected = selectCategoryDialog.getSelected().get(0);
+                FormField field = formFields.get(Finder.searchByFieldName("category_id", formFields));
+                field.setValue(Long.toString(selected));
+
+                List<CategoryTable.CategoryTableItem> categoryTableItems = categoryTable.getCategoryItems();
+                boolean find = false;
+                if(categoryTableItems != null && categoryTableItems.size() > 0) {
+                    for (CategoryTable.CategoryTableItem categoryTableItem : categoryTableItems) {
+                        if(categoryTableItem.getCategory().getId() == selected) {
+                            categoryTableItem.setSelectedView(true);
+                            find = true;
+                        } else {
+                            categoryTableItem.setSelectedView(false);
+                        }
+                    }
+                }
+
+                CardView categoryIcon = (CardView) selectCategoryButton.findViewById(R.id.categoryIcon);
+                if(!find) {
+                    categoryIcon.setCardBackgroundColor(context.getColor(R.color.active));
+                } else {
+                    categoryIcon.setCardBackgroundColor(context.getColor(R.color.normal));
+                }
+            }
+        });
     }
 
     public static class Builder extends Form.Builder {
